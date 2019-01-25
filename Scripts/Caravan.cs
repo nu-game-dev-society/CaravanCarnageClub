68b567831095cf331a3285c6097751bf655f1afd
Joseph Whitehead <joseph.whitehead@northumbria.ac.uk>
2019-01-25 22:15:52 +0000

    FIRST COMMIT

---
+++ Scripts/Caravan.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caravan : MonoBehaviour {
+
+    PlayerManager playerManager;
+
+    public int index;
+
+	// Use this for initialization
+	void Start () {
+        playerManager = GameObject.FindGameObjectWithTag("PlayerManager").GetComponent<PlayerManager>();
+        playerManager.AddCaravan(this);
+        index = playerManager.CountCaravans() - 1;
+        gameObject.name = "Caravan" + index;
+	}
+	
+	//Destroys this caravan @todo Make it explode and shit
+	public void DestroyCaravan ()
+    {
+        Destroy(gameObject);
+	}
+
+    
+    public void OnCollisionEnter(Collision collision)
+    {
+        //If it collides with something that should blow it the fuck up 
+        if (collision.gameObject.CompareTag("Something"))
+        {
+            playerManager.DestroyCaravan(index);
+        }
+    }
+}
---
+++ Scripts/PlayerManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public List<Caravan> myCaravans = new List<Caravan>();
+
+    public GameObject caravan;
+
+
+    public void CaravanCreationTest()
+    {
+        Instantiate(caravan);
+    }
+
+    //Adds a new caravan
+    public void AddCaravan(Caravan caravan)
+    {
+        myCaravans.Add(caravan);
+    }
+
+    public int CountCaravans()
+    {
+        return myCaravans.Count;
+    }
+
+    public void DestroyCaravan(int caravanIndex)
+    {
+        for (int i = caravanIndex; i < myCaravans.Count; i++)
+        {
+            myCaravans[i].DestroyCaravan();
+        }
+    }
+}
