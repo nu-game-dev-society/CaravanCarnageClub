5e0803e20f8bfe175d499c7475917f28e654b6f5
andrew.patton-1 <w16007006@northumbria.ac.uk>
2019-01-25 22:32:53 +0000

    hopeffully

---
+++ Scripts/Drunk.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drunk : MonoBehaviour {
+
+    public Material material;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
+}
---
+++ Scripts/Movement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private void Update()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (Input.GetKey(KeyCode.W)){rb.AddForce(Vector3.forward);}
+        if (Input.GetKey(KeyCode.S)){rb.AddForce(Vector3.back);}
+        if (Input.GetKey(KeyCode.A)){rb.AddForce(Vector3.left);}
+        if (Input.GetKey(KeyCode.D)){rb.AddForce(Vector3.right);}
+    }
+}
