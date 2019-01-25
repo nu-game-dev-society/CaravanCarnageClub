e41fdc7334bef528d159e764992afce20d302c63
ryan.milner <w18011433@northumbria.ac.uk>
2019-01-25 23:26:33 +0000

    Drunk powerup

--- Scripts/Drunk.cs
+++ Scripts/Drunk.cs
@@ -6,8 +6,72 @@ public class Drunk : MonoBehaviour {
 
     public Material material;
 
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float shakeAmount = 0.05f;
+
+    //Pulsate FOV Clamp (+/-)
+    public const float FOV_DELTA = 5f;
+
+    //Backup
+    Vector3 originalPos;
+    float originalFov;
+
+    public bool drunk = false;
+    float drunkTime = 0;
+
+    void Awake()
+    {
+        originalFov = Camera.main.fieldOfView;
+    }
+
+    void OnEnable()
+    {
+        originalPos = transform.localPosition;
+    }
+
+    void Update()
+    {
+        drunkTime -= Time.deltaTime;
+        if (drunkTime < 0)
+        {
+            drunk = false;
+        }
+
+        if (drunk)
+        {
+            transform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+            //Pulsate
+            //Camera.main.fieldOfView = Mathf.PingPong(originalFov + (Time.time*10), originalFov + FOV_DELTA);
+            Camera.main.fieldOfView = Mathf.Lerp(originalFov - FOV_DELTA, originalFov + FOV_DELTA, Mathf.PingPong(Time.time * 0.4f, 1));
+        }
+        else
+        {
+            transform.localPosition = originalPos;
+        }
+    }
+
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        Graphics.Blit(source, destination, material);
+        if (drunk)
+        {
+            Graphics.Blit(source, destination, material);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+
+    public void AddDrunkTime(int time)
+    {
+        if (drunkTime < 0)
+        {
+            drunkTime = time;
+        }
+        else
+        {
+            drunkTime += time;
+        }
     }
 }
---
+++ Scripts/Powerups/DrunkPower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrunkPower : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Camera.main.GetComponent<Drunk>().drunk = true;
+        Camera.main.GetComponent<Drunk>().AddDrunkTime(5);
+        Destroy(gameObject);
+    }
+}
