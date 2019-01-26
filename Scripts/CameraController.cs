f372a3cb16385577099802b3214b4f48a8b56a38
andrew.nattress <w17009550@northumbria.ac.uk>
2019-01-26 10:52:04 +0000

    cam script

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField]
+    Transform target;
+    [SerializeField]
+    float maxDist;
+
+	void LateUpdate () {
+        Vector3 camPos = transform.position;
+        Vector3 carPos = target.position;
+        float dist = Vector3.Distance(camPos, carPos);
+        if (dist > maxDist)
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(carPos.x, camPos.y, carPos.z), 5*Time.deltaTime);
+        transform.LookAt(carPos);
+	}
+}
