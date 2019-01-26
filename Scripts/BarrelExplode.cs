d638ff05d2fcb27e6e08ecaba6abd063b98b281c
ryan.milner <w18011433@northumbria.ac.uk>
2019-01-26 19:47:24 +0000

    Barrels!

---
+++ Scripts/BarrelExplode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelExplode : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag != "Terrain")
+        {
+            //Explode
+            Destroy(gameObject);
+        }
+    }
+}
