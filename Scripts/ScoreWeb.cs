67d35c0cd30a1a368d5338efe8a2b73434f9e918
ryan.milner <w18011433@northumbria.ac.uk>
2019-01-26 21:57:54 +0000

    Score web stuff

---
+++ Scripts/ScoreWeb.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[System.Serializable]
+public class Score
+{
+    public string name;
+    public int score;
+}
+
+[System.Serializable]
+public class ScoreData
+{
+    public Score[] scores;
+}
+
+public static class ScoreWeb
+{
+    public static void Send(string name, int score)
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://81.174.149.122/ggj/submit.php?name=" + name + "&score=" + score);
+        request.GetResponse();
+    }
+
+    public static Score[] Get()
+    {
+        string html = string.Empty;
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://81.174.149.122/ggj/get.php");
+        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        using (Stream stream = response.GetResponseStream())
+        using (StreamReader reader = new StreamReader(stream))
+        {
+            html = reader.ReadToEnd();
+        }
+        
+        return JsonUtility.FromJson<ScoreData>(html).scores;
+    }
+}
