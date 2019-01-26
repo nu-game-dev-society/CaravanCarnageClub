caf4e784df5a49ccec385663c600d4989f144006
Joseph Whitehead <joseph.whitehead@northumbria.ac.uk>
2019-01-26 20:55:09 +0000

    Slightly smarted ai, still retared though

--- Scripts/EnemyCar.cs
+++ Scripts/EnemyCar.cs
@@ -5,44 +5,30 @@ using UnityStandardAssets.Vehicles.Car;
 
 public class EnemyCar : MonoBehaviour {
 
-    [SerializeField] GameObject[] waypoints;
-    [SerializeField] CarAIControl AIControl;
     [SerializeField] GameObject Waypoint;
     [SerializeField] GameObject myCaravan;
 
-    Transform target;
-    float distToTarget;
+    [SerializeField] WaypointPathfinding wpPf;
+
+   
 
     private void Start()
     {
-        AIControl = gameObject.GetComponent<CarAIControl>();
-        waypoints = GameObject.FindGameObjectsWithTag("Waypoints");
-        target = waypoints[(Random.Range(0, waypoints.Length))].transform;
+
     }
 
     // Update is called once per frame
     void Update () {
 
-        Waypoint.transform.position = target.position;
-
-        if (target != null)
-        {
-            target = AIControl.getTarget();
-            distToTarget = Vector3.Distance(target.position, transform.position);
-        }
-        
-
-        if (distToTarget <= 5 || target == null)
-        {
-            target = waypoints[(Random.Range(0, waypoints.Length))].transform;
-        }
+        //Waypoint.transform.position = target.position;
 
 
 	}
 
     public void Boom()
     {
-        Destroy(myCaravan);
-        Destroy(gameObject);
+
+        Destroy(gameObject.transform.parent.gameObject);
+        
     }
 }
---
+++ Scripts/WaypointPathfinding.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPathfinding : MonoBehaviour {
+
+    [SerializeField] GameObject[] waypoints;
+    public Transform myTransform;
+    public Transform target;
+
+    public float distToTarget;
+
+    public Transform myCar;
+
+
+    [SerializeField] NavMeshAgent pathfinder;
+
+	// Use this for initialization
+	void Start () {
+        pathfinder = gameObject.GetComponent<NavMeshAgent>();
+        waypoints = GameObject.FindGameObjectsWithTag("Waypoints");
+        target = waypoints[(Random.Range(0, waypoints.Length))].transform;
+        UpdatePath();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pathfinder.SetDestination(target.position);
+
+        
+
+        if (target != null)
+        {
+            distToTarget = Vector3.Distance(target.position, transform.position);
+        }
+
+
+        if (distToTarget <= 5 || target == null)
+        {
+            target = waypoints[(Random.Range(0, waypoints.Length))].transform;
+        }
+
+    }
+
+    IEnumerator UpdatePath()
+    {
+        float refreshRate = .05f;
+
+        while (target != null)
+        {
+                pathfinder.SetDestination(target.position);
+                pathfinder.speed = 25;
+
+           
+            yield return new WaitForSeconds(refreshRate);
+        }
+
+        yield return new WaitForSeconds(3);
+      
+        StartCoroutine(UpdatePath());
+    }
+}
