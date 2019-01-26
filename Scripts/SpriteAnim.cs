28c1bcfcdbb8f1a69c3f9ff928796894500c3631
ryan.milner <w18011433@northumbria.ac.uk>
2019-01-26 14:49:42 +0000

    Added sprites

---
+++ Scripts/SpriteAnim.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnim : MonoBehaviour
+{
+    [SerializeField]
+    float frameTime = 0.5f;
+
+    float nextFrameTime = 0.0f;
+
+    [SerializeField]
+    Sprite[] images;
+
+    int currentFrame = 0;
+
+    SpriteRenderer spr;
+
+	// Use this for initialization
+	void Start () {
+        spr = GetComponent<SpriteRenderer>();
+        spr.sprite = images[currentFrame];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > nextFrameTime)
+        {
+            nextFrameTime += frameTime;
+
+            spr.sprite = images[currentFrame];
+
+            currentFrame++;
+            if (currentFrame > images.Length)
+            {
+                currentFrame = 0;
+            }
+        }
+	}
+}
