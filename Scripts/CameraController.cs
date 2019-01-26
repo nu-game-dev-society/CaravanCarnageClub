1865ca581d6c2069e113844569582df8d47a4018
andrew.nattress <w17009550@northumbria.ac.uk>
2019-01-26 11:10:23 +0000

    cam perfect <3

--- Scripts/CameraController.cs
+++ Scripts/CameraController.cs
@@ -7,13 +7,16 @@ public class CameraController : MonoBehaviour {
     Transform target;
     [SerializeField]
     float maxDist;
+    [SerializeField]
+    float camSpeed = 20;
 
 	void LateUpdate () {
         Vector3 camPos = transform.position;
         Vector3 carPos = target.position;
         float dist = Vector3.Distance(camPos, carPos);
         if (dist > maxDist)
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(carPos.x, camPos.y, carPos.z), 5*Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(carPos.x, camPos.y, carPos.z), camSpeed * Time.deltaTime);
         transform.LookAt(carPos);
+        print(dist);
 	}
 }
