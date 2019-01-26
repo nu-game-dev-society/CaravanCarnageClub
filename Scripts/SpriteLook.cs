40b4a8c8212c7a32a71a41b421f76cd33554fd6e
ryan.milner <w18011433@northumbria.ac.uk>
2019-01-26 16:08:03 +0000

    Sprite look and import settings

---
+++ Scripts/SpriteLook.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLook : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetPostition = Camera.main.transform.position;
+        targetPostition.x = transform.position.x;
+        transform.LookAt(targetPostition);
+	}
+}
