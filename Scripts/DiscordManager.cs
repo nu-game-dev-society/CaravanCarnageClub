0523b33fc36bad6a38888249999652c0f601cb48
Ryan Milner <CVGS18011433@HAL.local>
2019-01-30 20:01:00 +0000

    Discord intergration start

---
+++ Scripts/DiscordManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscordManager : MonoBehaviour {
+
+    private string applicationId = "540255683837296671";
+    private string steamId = "";
+    private DiscordRpc.RichPresence presence = new DiscordRpc.RichPresence();
+    private DiscordRpc.EventHandlers handlers;
+
+    void OnEnable () {
+        handlers = new DiscordRpc.EventHandlers();
+        handlers.readyCallback += ReadyCallback;
+        handlers.disconnectedCallback += DisconnectedCallback;
+        handlers.errorCallback += ErrorCallback;
+        DiscordRpc.Initialize(applicationId, ref handlers, true, steamId);
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("Discord: shutdown");
+        DiscordRpc.Shutdown();
+    }
+
+    void Update()
+    {
+        DiscordRpc.RunCallbacks();
+    }
+
+    public void ReadyCallback(ref DiscordRpc.DiscordUser connectedUser)
+    {
+        Debug.Log(string.Format("Discord: connected to {0}#{1}: {2}", connectedUser.username, connectedUser.discriminator, connectedUser.userId));
+
+        presence.details = "Shit happens?";
+        presence.largeImageKey = "logo";
+        DiscordRpc.UpdatePresence(presence);
+    }
+
+    public void DisconnectedCallback(int errorCode, string message)
+    {
+        Debug.Log(string.Format("Discord: disconnect {0}: {1}", errorCode, message));
+    }
+
+    public void ErrorCallback(int errorCode, string message)
+    {
+        Debug.Log(string.Format("Discord: error {0}: {1}", errorCode, message));
+    }
+
+}
