733c7a0fcfe66de91a6a75c5c8e1e768df981f92
Andrew Patton <CSG16007006@HAL.local>
2019-03-20 20:05:08 +0000

    HELLOOOO ROCKET

---
+++ Scripts/Object3DSpin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object3DSpin : MonoBehaviour {
+    //public float bounceSpeed = 2.0f; - REDACTED
+    public float spinSpeed   = 100.0f;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up, spinSpeed);
+        //transform.position =  new Vector3 (transform.position.x, Mathf.PingPong(Time.time * bounceSpeed, 5.0f), transform.position.z); - REDACTED
+    }
+}
