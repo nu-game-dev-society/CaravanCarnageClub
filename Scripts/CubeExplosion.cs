1e3e5c9f9373d3200b2d746a1df303f4db264b17
Andrew Patton <CSG16007006@HAL.local>
2019-04-03 20:19:00 +0100

    CUBES AND SCRIPTZ

--- Scripts/CubeExplosion.cs
+++ Scripts/CubeExplosion.cs
@@ -4,21 +4,62 @@ using UnityEngine;
 
 public class CubeExplosion : MonoBehaviour
 {
-    public Material material;
-    public float    speed;
-    private void OnDestroy()
+    public  Material material;
+    public  float    speed;
+    
+    static private List<GameObject> cubes;
+
+    private int iFrame = 0;
+    private float spacing = 1.0f;
+    private Vector3 parentPosition;
+
+    private void Start()
     {
+        parentPosition = gameObject.transform.position;
+        cubes = new List<GameObject>();        
+
         for (int i = 0; i < 100; i++)
         {
             GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube.transform.localPosition = new Vector3(transform.localPosition.x + Random.Range(-1.0f,1.0f), 
-                                                       transform.localPosition.y + Random.Range(-1.0f, 1.0f), 
-                                                       transform.localPosition.z + Random.Range(-1.0f, 1.0f));
             cube.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
             cube.GetComponent<Renderer>().material = material;
-            cube.AddComponent<Rigidbody>().velocity = new Vector3(Random.Range(-1.0f, 1.0f) * speed, 
+            cube.AddComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+            cube.transform.localPosition = parentPosition + new Vector3(Random.Range(-spacing, spacing), 
+                                                                        Random.Range(-spacing, spacing), 
+                                                                        Random.Range(-spacing, spacing));
+            cubes.Add(cube);
+        }
+    }
+
+    private void Update()
+    {
+        //TEMP: Explode on iFrame #300, reset on iFrame #450
+        if (iFrame == 300) Explode();
+        if (iFrame == 450) ResetCubes();
+        iFrame++;
+    }
+
+    private void ResetCubes()
+    {
+        foreach (GameObject cube in cubes)
+        {
+            cube.transform.localPosition = parentPosition + new Vector3(Random.Range(-spacing, spacing),
+                                                                        Random.Range(-spacing, spacing),
+                                                                        Random.Range(-spacing, spacing));
+
+            cube.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+        iFrame = 0;
+    }
+
+    private void Explode()
+    {
+        ResetCubes(); //Reset for safety before explosion
+        foreach(GameObject cube in cubes)
+        {   
+            cube.GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-1.0f, 1.0f) * speed, 
                                                                   Random.Range(-1.0f, 1.0f) * speed, 
-                                                                  Random.Range(-1.0f, 1.0f) * speed);
+                                                                  Random.Range(-1.0f, 1.0f) * speed);            
         }
     }
 }
---
+++ Scripts/ObjectManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    #region Singleton Pattern
+    public static ObjectManager Instance { get; private set; }
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+    #endregion
+
+    [Header("Object")]
+    public int maxInstances = 10;
+    public GameObject obj;
+
+    [Header("Position Constraints")]
+    public Vector3 minVal = Vector3.zero;
+    public Vector3 maxVal = Vector3.zero;
+
+    /**/
+    private List<GameObject> gameObjects;
+
+    private void Start()
+    {
+        for (int i = 0; i < maxInstances; i++)
+        {
+            GameObject newGO = Instantiate(obj, Vector3.zero, Quaternion.identity);
+
+            newGO.transform.localPosition = new Vector3(Random.Range(minVal.x, maxVal.x), Random.Range(minVal.y, maxVal.y), Random.Range(minVal.z, maxVal.z));
+
+            gameObjects.Add(newGO);
+            print(i);
+        }
+    }
+
+    void Update()
+    {
+        
+    }    
+}
