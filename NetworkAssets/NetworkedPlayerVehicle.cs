9b87f4f40dee1640ff12108156f5d947bc7d7bb4
Andrew Nattress <CSG17009550@HAL.local>
2019-10-09 19:56:51 +0100

    photon

---
+++ NetworkAssets/NetworkedPlayerVehicle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkedPlayerVehicle : MonoBehaviourPun, IPunObservable
+{
+
+    Vector3 targetPos;
+    Quaternion targetRot;
+
+    void Awake()
+    {
+        if(!photonView.IsMine)
+            Destroy(GetComponent<Vehicle>());
+    }
+    void Update()
+    {
+        if(!photonView.IsMine)
+        {
+            float interp = 10f * Time.deltaTime;
+            transform.position = Vector3.Lerp(transform.position, targetPos, interp);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, interp);
+        }
+    }
+    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)//is sending
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            targetPos = (Vector3)stream.ReceiveNext();
+            targetRot = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
